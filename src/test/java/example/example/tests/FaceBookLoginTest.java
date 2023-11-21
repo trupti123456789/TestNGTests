daca86fefc7bda9be7d58f812d855673846ac709
trupti123456789 <trupti.c@cloudfulcrum.com>
2023-11-21 20:01:02 +0530

    test

---
+++ src/test/java/example/example/tests/FaceBookLoginTest.java
@@ -0,0 +1,25 @@
+package example.example.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import example.example.factory.PageinstancesFactory;
+import example.example.pages.FacebookLoginPage;
+
+/**
+ * The Class FaceBookLoginTest.
+ *
+ * @author Bharathish
+ */
+@Test(testName = "Facebook login test", description = "Facebook login test")
+public class FaceBookLoginTest extends BaseTest {
+
+	@Test
+	public void facebookLoginTest() throws InterruptedException {
+		driver.get("https://test.salesforce.com/");
+		FacebookLoginPage facebookLoginPage = PageinstancesFactory.getInstance(FacebookLoginPage.class);
+		facebookLoginPage.enterEmail("vishnu.r@cloudfulcrum.com.trnprod").enterPassword("Qazwsx123").clickSignIn();
+		Thread.sleep(10);
+		Assert.assertTrue(true, "Login pass : Test pass");
+	}
+}
---
+++ src/test/java/example/example/tests/GoogleSearchTest.java
@@ -0,0 +1,29 @@
+package example.example.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import example.example.factory.PageinstancesFactory;
+import example.example.pages.GooglePage;
+
+/**
+ * The Class GoogleSearchTest.
+ *
+ * @author Bharathish
+ */
+@Test(testName = "Google search test", description = "Test description")
+public class GoogleSearchTest extends BaseTest {
+
+	/**
+	 * Google search test.
+	 */
+	@Test
+	public void googleSearchTest() {
+		
+		driver.get("https://www.google.co.in/");
+		GooglePage googlePage = PageinstancesFactory.getInstance(GooglePage.class);
+		googlePage.searchText("abc");
+		System.out.println("Hello World");
+		Assert.assertTrue(driver.getTitle().contains("abc"), "Title doesn't contain abc : Test Failed");
+	}
+}
